43db17230c709b44a6ac0698ecf65045f7687135
koseoglu7 <diyokoo21@gmail.com>
2023-11-29 14:26:09 -0600

    added size

--- main.tf
+++ main.tf
@@ -5,4 +5,15 @@ resource "kubernetes_namespace" "example" {
     name        = var.name
   }
 }
+resource "kubernetes_resource_quota" "example" {
+  metadata {
+    name = "pod-limit"
+  }
+  spec {
+    hard = {
+      pods = 1000
+    }
+    scopes = ["BestEffort"]
+  }
+}
 
